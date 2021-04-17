eabd970c4a969485b6317000d7ba1c55d2a9c486
imtu003 <ahmed.imti1994@gmail.com>
2021-04-17 15:37:26 +0600

    playing around with template literals

--- strOutput/templateLiterals/script.js
+++ strOutput/templateLiterals/script.js
@@ -4,7 +4,10 @@
  *
  */
 import Backpack from "./Backpack.js";
+import Book from "./Book.js";
 
+
+// Backpack object
 const everydayPack = new Backpack(
   "Everyday Pack",
   30,
@@ -16,8 +19,21 @@ const everydayPack = new Backpack(
   "December 5, 2018 15:00:00 PST"
 );
 
+// Book object
+const aBook = new Book (
+  'book title',
+  'Jon doe',
+  1245868,
+  "2019",
+  500,
+  250,
+  "reading"
+);
+
+
+// template literals
 const content = `
-  <main>
+  <div>
     <article>
       <h1>${everydayPack.name}</h1>
       <ul>
@@ -30,11 +46,42 @@ const content = `
         <li>Lid status: ${everydayPack.lidOpen}</li>
       </ul>
     </article>
-  </main>
+  </div>
 `;
 
-document.body.innerHTML = content;
+const secContent = `
+  <div>
+    <article>
+      <h1>Book</h1>
+      <ul>
+        <li>Title: ${aBook.title}</li>
+        <li>Author: ${aBook.author}</li>
+        <li>ISBN: ${aBook.ISBN}</li>
+        <li>Published Year: ${aBook.pubYear}</li>
+        <li>Page Number: ${aBook.pageNumber}</li>
+        <li>Current Page: ${aBook.currentPage}</li>
+        <li>Read status: ${aBook.readStatus}</li>
+      </ul>
+    </article>
+  </div>
+`;
 
+
+
+const first = document.createElement("first");
+first.innerHTML = content;
+
+const second = document.createElement("second");
+second.innerHTML = secContent;
+
+// document.body.innerHTML = content;
+// document.body.innerHTML = secContent;
+
+document.body.appendChild(first);
+document.body.appendChild(second);
+
+
+// For browser testing
 console.log("The everydayPack object:", everydayPack);
 console.log("The pocketNum value:", everydayPack.pocketNum);
 console.log("Days since aquired:", everydayPack.backpackAge());
