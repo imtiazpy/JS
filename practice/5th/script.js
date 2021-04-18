ea50ce65780c1ada1432d32c393f07bb5fb49539
imtu003 <ahmed.imti1994@gmail.com>
2021-04-18 19:03:05 +0600

    practicing finding element

---
+++ practice/5th/script.js
@@ -0,0 +1,23 @@
+// Practicing finding Element
+
+
+// console.log(document.querySelector(".siteheader"));
+
+const main = document.querySelector(".maincontent"); //class
+
+// const p = document.querySelector("main p"); //(element element)
+
+// const content = `<h1>testing</h1>`;
+
+// main.innerHTML = content;
+
+// main.p.style.backgroundColor = "red";
+
+document.body.style.backgroundColor = "orange";
+
+document.getElementById("pack01").style.backgroundColor = "lightblue";
+document.getElementById("pack02").style.backgroundColor = "olive";
+
+
+const backpack = document.querySelectorAll(".backpack")
+
