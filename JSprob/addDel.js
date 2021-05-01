d26d963123481ffe441d6ebb84439ec812533ba1
imtu003 <ahmed.imti1994@gmail.com>
2021-05-01 13:39:57 +0600

    Updated the problem file

--- JSprob/addDel.js
+++ JSprob/addDel.js
@@ -1,11 +1,12 @@
+const names = []
+
 // write a function to add name to an emptly array.
 
-const names = [];
 
 function addName() {
     const name = prompt("Name to add.");
     names.push(name);
-    console.log(names);
+   // console.log(names);
 }
 
 
@@ -20,5 +21,44 @@ function delName() {
     } else {
         alert("Name not found!");
       }
+   // console.log(names);
+}
+
+// Display the names of the array.
+
+function display() {
     console.log(names);
 }
+
+
+// write a function to call the methods according the user choice.
+
+function play() {
+    const start = prompt("Would you like to use the web app? (y/n) ");
+    let req = "empty";
+    
+    if (start === 'y') {
+        while (req !== 'quit') {
+            req = prompt("Pls select an option: add, remove, display or quit");
+            if (req === 'add') {
+                addNew();
+            } else if (req === 'remove') {
+                delName();
+            } else if (req === 'display') {
+                display();
+            } else if (req === 'quit') {
+                req = 'quit';
+            } else {
+                alert('request not recognized, will quit');
+                req = 'quit';
+            }
+        }
+        alert("Thanks for using the app, refresh the page to start over");
+    } else {
+        alert("Never mind")
+    }
+    
+}
+
+
+play()
